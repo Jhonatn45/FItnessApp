63f7afb25e6033a80e7d43d0aa92401181732dd2
Jhomove <ing.jhonatanm@gisoft.co>
2020-02-02 12:40:25 -0500

    Primera vista de como usar Jsx

--- src/index.js
+++ src/index.js
@@ -1,19 +1,3 @@
-//Hola mundo con js nativo
-// const element = document.createElement('h1');
-// element.innerText = 'Hola React';
-// const container = document.getElementById('root');
-// container.appendChild(element);
-
-//Hola mundo con React js
-// import React from 'react'
-// import ReactDOM from 'react-dom'
-
-// const element = <h1>Hola React</h1>
-// const container = document.getElementById('root')
-
-// //ReactDOM.render(__QUE__,__DONDE__);
-// ReactDOM.render(element,container)
-
 //Uso de jsx
 
 import React from 'react'
@@ -47,3 +31,4 @@ const element = (
 const container = document.getElementById('root')
 
 ReactDOM.render(element,container)
+// //ReactDOM.render(__QUE__,__DONDE__);
