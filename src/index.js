de3bb7ebd6d47dc624ab8931b5267ecdad7b0c00
Jhomove <ing.jhonatanm@gisoft.co>
2020-02-02 12:37:15 -0500

    Curso

--- src/index.js
+++ src/index.js
@@ -1,12 +1,49 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+//Hola mundo con js nativo
+// const element = document.createElement('h1');
+// element.innerText = 'Hola React';
+// const container = document.getElementById('root');
+// container.appendChild(element);
 
-ReactDOM.render(<App />, document.getElementById('root'));
+//Hola mundo con React js
+// import React from 'react'
+// import ReactDOM from 'react-dom'
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// const element = <h1>Hola React</h1>
+// const container = document.getElementById('root')
+
+// //ReactDOM.render(__QUE__,__DONDE__);
+// ReactDOM.render(element,container)
+
+//Uso de jsx
+
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const user = {
+    name: 'Jhonatan',
+    lastName: 'Moreno',
+    gender: 'male',
+    avatar: 'https://cl.buscafs.com/www.levelup.com/public/uploads/images/533088_1140x516.jpg'
+}
+
+function getName(user){
+    return `${user.name} ${user.lastName}`
+}
+
+function getGreeting(user){
+    if(user)
+        return <h1>Hello {user.gender === 'male' ? 'Mr.' : 'Ms.'} {getName(user)}</h1>
+    else
+        return <h1>Hello Stranger</h1>
+}
+
+const element = (
+    <div>
+        {getGreeting(user)}
+        <img src={user.avatar}/>
+    </div>
+)
+
+const container = document.getElementById('root')
+
+ReactDOM.render(element,container)
