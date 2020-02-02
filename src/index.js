8b29739c5156c8e72bef14b62935683b525966d0
Jhomove <ing.jhonatanm@gisoft.co>
2020-02-02 12:39:08 -0500

    Hola react

--- src/index.js
+++ src/index.js
@@ -5,45 +5,11 @@
 // container.appendChild(element);
 
 //Hola mundo con React js
-// import React from 'react'
-// import ReactDOM from 'react-dom'
-
-// const element = <h1>Hola React</h1>
-// const container = document.getElementById('root')
-
-// //ReactDOM.render(__QUE__,__DONDE__);
-// ReactDOM.render(element,container)
-
-//Uso de jsx
-
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-const user = {
-    name: 'Jhonatan',
-    lastName: 'Moreno',
-    gender: 'male',
-    avatar: 'https://cl.buscafs.com/www.levelup.com/public/uploads/images/533088_1140x516.jpg'
-}
-
-function getName(user){
-    return `${user.name} ${user.lastName}`
-}
-
-function getGreeting(user){
-    if(user)
-        return <h1>Hello {user.gender === 'male' ? 'Mr.' : 'Ms.'} {getName(user)}</h1>
-    else
-        return <h1>Hello Stranger</h1>
-}
-
-const element = (
-    <div>
-        {getGreeting(user)}
-        <img src={user.avatar}/>
-    </div>
-)
-
+const element = <h1>Hola React</h1>
 const container = document.getElementById('root')
 
+//ReactDOM.render(__QUE__,__DONDE__);
 ReactDOM.render(element,container)
